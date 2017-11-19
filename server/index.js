7e8e8e77dc8f07356d3911bc3659fc21cc73672a
Alvin Kong <alvink92@gmail.com>
2017-11-19 14:19:29 -0800

    configed google auth + passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.clientID.google,
+            clientSecret: keys.clientSecret.google,
+            callbackURL: '/auth/google/callback'
+        }, (accessToken) => {
+        console.log(accessToken);
+        }
+    )
+);
 
-app.get("/", (req, res) => {
-    res.send({hi: 'there'});
-});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
