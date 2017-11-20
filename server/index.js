833c240cd7f9516b91b98a7e459d9d250cd790af
Alvin Kong <alvink92@gmail.com>
2017-11-20 13:49:50 -0800

    imported users model in index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-require('./services/passport');
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+require("./services/passport");
+require("./models/Users");
 
 // connect mongoose to our mongodb database
 console.log(keys.mongoURI);
