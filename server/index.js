3086af9e15b1dcaacb1756f8413ba7dcfc8b66fd
Alvin Kong <alvink92@gmail.com>
2017-11-20 14:28:15 -0800

    added cookie session to index

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 
 require("./models/User");
 require("./services/passport");
 
 // connect mongoose to our mongodb database
-console.log(keys.mongoURI);
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
 
 // connects authRoutes to app
 authRoutes(app);
