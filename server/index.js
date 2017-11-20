b3365ca053996cc7135d33d2c582ee1060245474
Alvin Kong <alvink92@gmail.com>
2017-11-19 22:07:44 -0800

    connect mongodb to mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const authRoutes = require("./routes/authRoutes");
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+// connect mongoose to our mongodb database
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+// connects authRoutes to app
 authRoutes(app);
 
-
+// dynamic PORT address
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
