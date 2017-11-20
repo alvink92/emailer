0f9449bd81e6042bda159c45ac3a4c2aec279379
Alvin Kong <alvink92@gmail.com>
2017-11-20 14:23:48 -0800

    deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,14 +1,22 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
-// adds intermediate step to serialized token(based on user.id - mongodb id) 
+// adds intermediate step to serialized token(based on user.id - mongodb id)
 // to user browser on login/signup
 passport.serializeUser((user, done) => {
   done(null, user.id);
+});
+
+// add intermediate step to deserialize token
+// passed to server on request to return user
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -19,15 +27,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id }).save()
-              .then(newUser => done(null, newUser));
-          }
-        });
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
+        }
+      });
     }
   )
 );
