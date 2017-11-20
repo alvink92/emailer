94a4c3eb1b5bd1cd4a986ea1f63f2e935be47a85
Alvin Kong <alvink92@gmail.com>
2017-11-19 21:41:31 -0800

    added gitignore to backend

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,15 +3,16 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.clientID.google,
-            clientSecret: keys.clientSecret.google,
-            callbackURL: '/auth/google/callback'
-        }, (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile)
-        }
-    )
+  new GoogleStrategy(
+    {
+      clientID: keys.clientID.google,
+      clientSecret: keys.clientSecret.google,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+    }
+  )
 );
