936f9f361d8e7f564f93b99ad1804058dc79fd27
Alvin Kong <alvink92@gmail.com>
2017-11-21 13:59:11 -0800

    made materialize header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" className="brand-logo">
+            Emailer
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="">Login with Google</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
